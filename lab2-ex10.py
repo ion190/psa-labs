f474704c0f28be90e732bdde5435725cd978ebcd
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 20:28:44 +0200

    Create lab2-ex10.py

---
+++ lab2-ex10.py
@@ -0,0 +1,3 @@
+# Jora Petrovici never pays the troleibuz (cost = 6 lei in Chisinau). He assumes that there is a probability of 0.05 that he will be caught by the controlor. The first offense costs 50 lei, the second costs 200 lei, and subsequent offenses cost 300 lei. There’s also an empirically proven probability of 0.02 that the taxatoarea is a hairy muscular guy, which means that he’ll have to pay the 6 lei anyway. How does the expected cost of riding the troleibuz for a year (two times a day) without paying compare to the cost that is paid by us, law-abiding students?
+
+
