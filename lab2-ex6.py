2016a79893fe3d4ca36e4d982e104f65d3084cbd
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 20:26:26 +0200

    Create lab2-ex6.py

---
+++ lab2-ex6.py
@@ -0,0 +1,2 @@
+# At the Ziua Vinului, a coin toss game works as follows. Coins of 25 bani are tossed onto a checkerboard. The management keeps all the coins, but for each coin landing entirely within one square of the checkerboard the management pays 1 leu. Assume that the edge of each square is twice the diameter of a coin, and that the outcomes are described by coordinates chosen at random. Is this a fair game?
+
