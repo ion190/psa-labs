636afe96ccd838e2d58c3ceece2b92eb92d3ce8e
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 20:25:16 +0200

    Create lab2-ex3.py

---
+++ lab2-ex3.py
@@ -0,0 +1,3 @@
+# Take a stick of unit length and break it into two pieces, choosing the breakpoint at random. Now break the longer of the two pieces at a random point. What is the probability that the three pieces can be used to form a triangle?
+# Write a computer program that would simulate the experiment. Explain your results.
+
