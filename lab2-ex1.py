18747e115ee8d9a234ece51e2fdfb4d25a93bd13
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 20:01:46 +0200

    Create lab2-ex1.py

---
+++ lab2-ex1.py
@@ -0,0 +1,2 @@
+# One student during a probability lecture noticed that although the number of faces of dice with integers from 1 to 6 with total 9 is like the number of such triples with value 10, a 9 seemed to come up less frequently than a 10 when three dice were rolled. Write a program to verify if the student was correct. For better proof, plot a graph with results.
+
