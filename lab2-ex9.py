5ee906c12134b5db0487e649bbba4527cf680d70
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 20:28:18 +0200

    Create lab2-ex9.py

---
+++ lab2-ex9.py
@@ -0,0 +1,3 @@
+# A game is played as follows: A random number X is chosen uniformly from [0, 1]. Then a sequence Y1, Y2, . . . of random numbers is chosen independently and uniformly from [0, 1]. The game ends the first time that Yi > X. You are then paid (i - 1) dollars. What is a fair entrance fee for this game?
+
+
