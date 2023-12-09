e99092c6d404d3a7362231fe37ddb5498a2cab1f
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 20:25:52 +0200

    Create lab2-ex5.py

---
+++ lab2-ex5.py
@@ -0,0 +1,3 @@
+# I'm sure you've all seen cases where in a family the relatives want the child to have a certain gender. Lets estimate, by simulation, the average number of children there would be in a family if all people had children until they had a boy. Do the same if all people had children until they had at least one boy and at least one girl. How many more children would you expect to find under the second scheme that under the first in 100,000 families?
+
+
