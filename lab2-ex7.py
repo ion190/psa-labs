5f085373eb47503ec8e3d8e6b875f2056e25a38a
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 20:26:58 +0200

    Create lab2-ex7.py

---
+++ lab2-ex7.py
@@ -0,0 +1,2 @@
+# Write a program to carry out the following experiment. A coin is tossed 100 times and the number of heads that turn up is recorded. This experiment is then repeated 1000 times. Have your program plot a bar graph for the proportion of the 1000 experiments in which the number of heads is n, for each n in the interval [35, 65]. Does the bar graph look as though it can be fit with a normal curve?
+
