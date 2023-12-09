5e006d0e82cf8db9551d3fd48eafb7cd501cc4d5
none190 <145373308+none190@users.noreply.github.com>
2023-12-09 20:27:25 +0200

    Create lab2-ex8.py

---
+++ lab2-ex8.py
@@ -0,0 +1,3 @@
+# At a mathematical conference, 10 participants are randomly seated around a circular table for meals. Using simulation, estimate the probability that no two people sit next to each other at both lunch and dinner. Can you make an intelligent conjecture for the case of n participants when n is large?
+
+
